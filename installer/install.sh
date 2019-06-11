8b277baabd33fe8c038289fe651519e1b31201e7
Tim <timzhong2374@hotmail.co.uk>
2019-06-11 07:33:30 -0400

    Updates installer script

--- installer/install.sh
+++ installer/install.sh
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 # HELPER TO PRINT STEP HEAPER
 printHeader () {
   printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
@@ -19,14 +20,15 @@ printHeader () {
 }
 
 # REMOVE EXISTING REST SERVER, PLAYGROUND ETC
-docker rm -f $(docker ps -a | grep hyperledger/* | awk '{ print $1 }')
+printHeader "REMOVE EXISTING REST SERVER, PLAYGROUND ETC"
 
-docker pull hyperledger/composer-playground:latest
-docker pull hyperledger/composer-cli:latest
-docker pull hyperledger/composer-rest-server:latest
-docker pull hyperledger/letters-of-credit:latest
+lsof -n -i:8080 | grep LISTEN | awk '{ print $2 }' | xargs kill
+lsof -n -i:3000 | grep LISTEN | awk '{ print $2 }' | xargs kill
 
-#DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+docker kill $(docker ps -q)
+docker rm $(docker ps -aq)
+docker rmi $(docker images dev-* -q)
+
 DIR="$(pwd)"
 
 # GET AND SETUP FABRIC
@@ -43,9 +45,20 @@ tar -xvf $DIR/fabric-tools/fabric-dev-servers.tar.gz
 
 $DIR/fabric-tools/stopFabric.sh
 $DIR/fabric-tools/teardownFabric.sh
+$DIR/fabric-tools/downloadFabric.sh
 $DIR/fabric-tools/startFabric.sh
+$DIR/fabric-tools/createPeerAdminCard.sh
+
+
+# DELETE ANY EXISTING BUSINESS NETWORK CARDS 
+printHeader "DELETE ANY EXISTING BUSINESS NETWORK CARDS"
+rm -fr ~/.composer
+composer card delete -c PeerAdmin@fabric-network
+composer card delete -c admin@rec-biznet
+echo "Don't worry if there're errors here. The cards we're deleting might not exist."
 
 cd $DIR
+
 
 # CREATE PEER ADMIN CARD AND IMPORT
 printHeader "CREATE PEER ADMIN CARD AND IMPORT"
@@ -54,99 +67,88 @@ rm -rf $(pwd)/.loc-card-store
 mkdir $(pwd)/.loc-card-store
 chmod 777 $(pwd)/.loc-card-store
 
+
 # CREATE CONNECTION PROFILE
 rm -fr $(pwd)/loc-stage
 mkdir $(pwd)/loc-stage
 chmod 777 $(pwd)/loc-stage
 echo '{
-	"name": "hlfv1",
-	"version": "1.0.0",
-	"client": {
-		"organization": "Org1",
-		"connection": {
-			"timeout": {
-				"peer": {
-					"endorser": "300",
-					"eventHub": "300",
-					"eventReg": "300"
-				},
-				"orderer": "300"
-			}
-		}
-	},
-	"orderers": {
-		"orderer.example.com": {
-			"url": "grpc://orderer.example.com:7050",
-			"grpcOptions": {}
-		}
-	},
-	"peers": {
-		"peer0.org1.example.com": {
-			"url": "grpc://peer0.org1.example.com:7051",
-			"eventUrl": "grpc://peer0.org1.example.com:7053",
-			"grpcOptions": {},
-			"endorsingPeer": true,
-			"chaincodeQuery": true,
-			"ledgerQuery": true,
-			"eventSource": true
-		}
-	},
-	"channels": {
-		"composerchannel": {
-			"orderers": ["orderer.example.com"],
-			"peers": {
-				"peer0.org1.example.com": {}
-			}
-		}
-	},
-	"certificateAuthorities": {
-		"ca.org1.example.com": {
-			"url": "http://ca.org1.example.com:7054",
-			"caName": "ca.org1.example.com"
-		}
-	},
-	"organizations": {
-		"Org1": {
-			"mspid": "Org1MSP",
-			"peers": ["peer0.org1.example.com"],
-			"certificateAuthorities": ["ca.org1.example.com"]
-		}
-	},
-	"x-type": "hlfv1",
-	"x-commitTimeout": 100
+    "name": "fabric-network",
+    "x-type": "hlfv1",
+    "version": "1.0.0",
+    "peers": {
+        "peer0.org1.example.com": {
+            "url": "grpc://localhost:7051"
+        }
+    },
+    "certificateAuthorities": {
+        "ca.org1.example.com": {
+            "url": "http://localhost:7054",
+            "caName": "ca.org1.example.com"
+        }
+    },
+    "orderers": {
+        "orderer.example.com": {
+            "url": "grpc://localhost:7050"
+        }
+    },
+    "organizations": {
+        "Org1": {
+            "mspid": "Org1MSP",
+            "peers": [
+                "peer0.org1.example.com"
+            ],
+            "certificateAuthorities": [
+                "ca.org1.example.com"
+            ]
+        }
+    },
+    "channels": {
+        "composerchannel": {
+            "orderers": [
+                "orderer.example.com"
+            ],
+            "peers": {
+                "peer0.org1.example.com": {
+                    "endorsingPeer": true,
+                    "chaincodeQuery": true,
+                    "eventSource": true
+                }
+            }
+        }
+    },
+    "client": {
+        "organization": "Org1",
+        "connection": {
+            "timeout": {
+                "peer": {
+                    "endorser": "300",
+                    "eventHub": "300",
+                    "eventReg": "300"
+                },
+                "orderer": "300"
+            }
+        }
+    }
 }' > $(pwd)/loc-stage/connection.json
 
-# CREATE PEER ADMIN CARD AND IMPORT
-docker run \
-  --rm \
-  --network composer_default \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  -v $(pwd)/loc-stage:/home/composer/loc-stage \
-  -v $(pwd)/fabric-tools/fabric-scripts/hlfv1/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp:/home/composer/PeerAdmin \
-  hyperledger/composer-cli:latest \
-  card create -p loc-stage/connection.json -u PeerAdmin -r PeerAdmin -r ChannelAdmin -f /home/composer/loc-stage/PeerAdmin.card -c PeerAdmin/signcerts/Admin@org1.example.com-cert.pem -k PeerAdmin/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk
 
-docker run \
-  --rm \
-  --network composer_default \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  -v $(pwd)/loc-stage:/home/composer/loc-stage \
-  -v $(pwd)/fabric-tools/fabric-scripts/hlfv1/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp:/home/composer/PeerAdmin \
-  hyperledger/composer-cli:latest \
-  card import -f /home/composer/loc-stage/PeerAdmin.card
+# CREATE A BUSINESS NETWORK CARD FOR THE HYPERLEDGER FABRIC ADMIN
+printHeader "CREATE A BUSINESS NETWORK CARD FOR THE HYPERLEDGER FABRIC ADMIN"
+ADMIN_DIR="$DIR/fabric-tools/fabric-scripts/hlfv12/composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"
+composer card create \
+-p $(pwd)/loc-stage/connection.json \
+-u PeerAdmin \
+-c $ADMIN_DIR/signcerts/Admin@org1.example.com-cert.pem \
+-k $ADMIN_DIR/keystore/114aab0e76bf0c78308f89efc4b8c9423e31568da0c340ca187a9b17aa9a4457_sk \
+-r PeerAdmin \
+-r ChannelAdmin
 
-# START PLAYGROUND
-printHeader "START PLAYGROUND"
-docker run \
-  -d \
-  --network composer_default \
-  --name composer \
-  -p 8080:8080 \
-  hyperledger/composer-playground:latest
 
-# WAIT FOR PLAYGROUND TO WAKE UP
-echo "WAIT FOR PLAYGROUND TO WAKE UP"
-sleep 5
+# IMPORT THE BUSINESS NETWORK CARD FOR THE HYPERLEDGER FABRIC ADMIN
+printHeader "IMPORT THE BUSINESS NETWORK CARD FOR THE HYPERLEDGER FABRIC ADMIN"
+composer card import -f PeerAdmin@fabric-network.card
+
 
 # PACKAGE THE BUSINESS NETWORK
 printHeader "PACKAGE THE BUSINESS NETWORK"
@@ -155,66 +157,44 @@ composer archive create -a rec-biznet.bna -t dir -n .
 cd $DIR
 cp $DIR/biznet/rec-biznet.bna rec-biznet.bna
 
+
 # INSTALL THE BNA
 printHeader "INSTALL THE BNA"
-docker run \
-  --rm \
-  --network composer_default \
-  -v $(pwd)/rec-biznet.bna:/home/composer/rec-biznet.bna \
-  -v $(pwd)/loc-stage:/home/composer/loc-stage \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  hyperledger/composer-cli:latest \
-  network install -c PeerAdmin@hlfv1 -a rec-biznet.bna
+composer network install -c PeerAdmin@fabric-network -a rec-biznet.bna
 
 NETWORK_VERSION=$(grep -o '"version": *"[^"]*"' $DIR/biznet/package.json | grep -o '[0-9]\.[0-9]\.[0-9]')
 
+
 # START THE BNA
 printHeader "START THE BNA"
-docker run \
-  --rm \
-  --network composer_default \
-  -v $(pwd)/rec-biznet.bna:/home/composer/rec-biznet.bna \
-  -v $(pwd)/loc-stage:/home/composer/loc-stage \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  hyperledger/composer-cli:latest \
-  network start -n rec-biznet -V $NETWORK_VERSION -c PeerAdmin@hlfv1 -A admin -S adminpw -f /home/composer/loc-stage/bnaadmin.card
+composer network start \
+--networkName rec-biznet \
+--networkVersion $NETWORK_VERSION \
+-A admin \
+-S adminpw \
+-c PeerAdmin@fabric-network
 
-docker run \
-  --rm \
-  --network composer_default \
-  -v $(pwd)/loc-stage:/home/composer/loc-stage \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  hyperledger/composer-cli:latest \
-  card import -f /home/composer/loc-stage/bnaadmin.card
 
-# CREATE THE NEEDED PARTICIPANTS
-# docker run \
-#   --rm \
-#   --network composer_default \
-#   -v $(pwd)/.loc-card-store:/home/composer/.composer \
-#   hyperledger/composer-cli:latest \
-#   transaction submit -c admin@rec-biznet -d '{"$class": "org.rec.loc.CreateDemoParticipants"}'
+# IMPORT THE BUSINESS NETWORK CARD FOR THE BUSINESS NETWORK ADMIN
+printHeader "IMPORT THE BUSINESS NETWORK CARD FOR THE BUSINESS NETWORK ADMIN"
+composer card import -f admin@rec-biznet.card
 
-# SET CORRECT PERMISSIONS
-printHeader "SET CORRECT PERMISSIONS"
-docker exec \
-  composer \
-  find /home/composer/.composer -name "*" -exec chmod 777 {} \;
 
-# START THE REST SERVER
-printHeader "START THE REST SERVER"
-docker run \
-  -d \
-  --network composer_default \
-  --name rest \
-  -v $(pwd)/.loc-card-store:/home/composer/.composer \
-  -e COMPOSER_CARD=admin@rec-biznet \
-  -e COMPOSER_NAMESPACES=never \
-  -p 3000:3000 \
-  hyperledger/composer-rest-server:latest
+# TEST CONNECTION
+printHeader "TEST CONNECTION"
+composer network ping -c admin@rec-biznet
 
-#WAIT FOR REST SERVER TO WAKE UP
-echo "WAIT FOR REST SERVER TO WAKE UP"
+# START PLAYGROUND
+printHeader "START PLAYGROUND"
+composer-playground &
+echo "WAIT FOR PLAYGROUND TO WAKE UP"
+sleep 5
+
+
+# START REST API
+printHeader "START REST API"
+composer-rest-server -c admin@rec-biznet -n never -w true &
+echo "WAIT FOR REST API TO WAKE UP"
 sleep 10
 
 
@@ -228,16 +208,13 @@ sleep 10
 # hyperledger/letters-of-credit:latest
 
 
-#WAIT FOR REACT SERVER TO WAKE UP
-# sleep 10
-
 # OPEN THE APPLICATION
-URLS="http://localhost:8080 http://localhost:3000/explorer/"
+URLS="http://localhost:3000/explorer/"
 case "$(uname)" in
 "Darwin") open ${URLS}
           ;;
 "Linux")  if [ -n "$BROWSER" ] ; then
-	       	        $BROWSER http://localhost:8080 http://localhost:3000/explorer/
+	       	        http://localhost:3000/explorer/
 	        elif    which x-www-browser > /dev/null ; then
                   nohup x-www-browser ${URLS} < /dev/null > /dev/null 2>&1 &
           elif    which xdg-open > /dev/null ; then
@@ -245,7 +222,7 @@ case "$(uname)" in
                           xdg-open ${URL}
 	                done
           elif  	which gnome-open > /dev/null ; then
-	                gnome-open http://localhost:8080 http://localhost:3000/explorer/
+	                gnome-open http://localhost:3000/explorer/
 	        else
     	            echo "Could not detect web browser to use - please launch Application and Composer Playground URL using your chosen browser ie: <browser executable name> http://localhost:8080 or set your BROWSER variable to the browser launcher in your PATH"
 	        fi
