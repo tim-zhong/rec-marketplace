9ecf014088cf54a8ea5e9c70cd3495a280471045
Tim <timzhong2374@hotmail.co.uk>
2019-06-28 18:27:59 -0400

    Sets up less so we can override styles from antd

---
+++ ui/config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            // @teal1 in constants.less
+            '@primary-color': '#35807E',
+        },
+    }),
+);
