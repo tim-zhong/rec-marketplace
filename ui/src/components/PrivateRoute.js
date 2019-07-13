120b6c3f4e604ea792dab186b2272d0b6b9688e8
Tim <timzhong2374@hotmail.co.uk>
2019-07-13 18:53:55 -0400

    Allows passing props to private components

--- ui/src/components/PrivateRoute.js
+++ ui/src/components/PrivateRoute.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest}) => (
+const PrivateRoute = ({ component: Component, componentProps, ...rest}) => (
     <Route {...rest} render={props=> (
         localStorage.getItem('user')
-            ? <Component {...props} />
+            ? <Component {...props} {...componentProps} />
             : <Redirect to={{ pathname: '/login', state: { from: props.location }}} />
     )} />
 );
